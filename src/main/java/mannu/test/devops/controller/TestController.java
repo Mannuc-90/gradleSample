7b1f1e6d3a4d3265f078dc1e002a9d1c353bce2a
Mannuc-90 <manpreet.c1990@gmail.com>
2022-09-11 19:44:42 +0530

    Update TestController.java

--- src/main/java/mannu/test/devops/controller/TestController.java
+++ src/main/java/mannu/test/devops/controller/TestController.java
@@ -8,6 +8,6 @@ public class TestController {
 
 	@GetMapping("/test")
 	public String test() {
-		return "This is a test gradle endpoint for DevOps learning - edit 1";
+		return "This is a test gradle endpoint for DevOps learning - edit 2";
 	}
 }
